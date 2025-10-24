2ade57e9c73739319b32147a072d5ad506fe785a
Tony De Corso <tony.dcorso@gmail.com>
2025-10-24 22:52:01 +0200

    Text rendering (8x8 bitmap)

--- examples/kute_wasm32.c
+++ examples/kute_wasm32.c
@@ -14,7 +14,5 @@ void wasm_update(int t)
 {
     (void) t;
     kute_clear(pixels, WIDTH, HEIGHT, kute_rgba(22, 22, 22, 255));
-    kute_fill_triangle(pixels, WIDTH, HEIGHT, 385, 400, 415, 400, 400, 425, kute_rgba(175, 0, 0, 255));
-    kute_fill_triangle(pixels, WIDTH, HEIGHT, 385, 200, 415, 200, 400, 175, kute_rgba(175, 0, 0, 255));
-    kute_fill_circle(pixels, WIDTH, HEIGHT, 400, 300, 50, kute_rgba(0, 0, 0, 128));
+    kute_draw_text(pixels, WIDTH, HEIGHT, 200, 200, "Hello from Kute!", kute_rgba(255, 255, 255, 255), 3);
 }
--- include/kute.h
+++ include/kute.h
@@ -14,4 +14,9 @@ void kute_fill_circle(uint32_t *pixels, int pw, int ph, int cx, int cy, int radi
 void kute_draw_line(uint32_t *pixels, int pw, int ph, int x0, int y0, int x1, int y1, uint32_t color);
 
 void kute_fill_triangle(uint32_t *pixels, int pw, int ph, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+
+void kute_draw_char(uint32_t *pixels, int pw, int ph, int x0, int y0, char c, uint32_t color, int scale);
+
+void kute_draw_text(uint32_t *pixels, int pw, int ph, int x0, int y0, const char* text, uint32_t color, int scale);
+
 #endif // H_KUTE
