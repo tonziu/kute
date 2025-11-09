3b673a16f31222857c0dc1b9fecc1db4581dd701
Tony De Corso <tony.dcorso@gmail.com>
2025-11-09 12:32:07 +0100

    Added devlog website

---
+++ docs/js/wasm_integration.js
@@ -0,0 +1,35 @@
+async function InitCanvas(canvasId, wasmPath) {
+    const response = await fetch(wasmPath);
+    const bytes = await response.arrayBuffer();
+
+    const { instance } = await WebAssembly.instantiate(bytes, {
+        env: { tanf: Math.tan}
+    });
+
+    let canvas = document.getElementById(canvasId);
+    canvas.width = instance.exports.wasm_get_width();
+    canvas.height = instance.exports.wasm_get_height();
+
+    const ptr = instance.exports.wasm_get_pixels();
+    const pixels = new Uint32Array(
+        instance.exports.memory.buffer,
+        ptr,
+        canvas.width * canvas.height
+    );
+
+    const ctx = canvas.getContext('2d');
+
+    function loop() {
+        instance.exports.wasm_loop();
+
+        const imageData = new ImageData(
+            new Uint8ClampedArray(pixels.buffer, pixels.byteOffset, pixels.byteLength),
+            canvas.width,
+            canvas.height
+        );
+        ctx.putImageData(imageData, 0, 0);
+        requestAnimationFrame(loop);
+    }
+
+    loop();
+}
